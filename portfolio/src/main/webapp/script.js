185db28260488c2f091f6ed8e482cff4fa46f87d
Sheryl <iamsherylng@gmail.com>
2020-06-12 15:10:17 +0800

    Edits made based on comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -30,24 +30,25 @@ function addRandomQuote() {
   const quoteContainer = document.getElementById('quote-container');
   quoteContainer.innerText = quote;
 }
+
 window.onload = function () {
-    startTab(); }
+    startTab();
+     }
 
 function startTab() {
-    document.getElementById("defaultOpen").click();}
+    document.getElementById("defaultOpen").click();
+    }
     
 function openPage(pageName, elmnt, color) {
-  
   // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  for (var i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
   // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
+  var tablinks = document.getElementsByClassName("tablink");
+  for (var i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
 
