f492d17487b100d09ad50082406386c16ea747fc
Sheryl <iamsherylng@gmail.com>
2020-06-14 23:33:14 +0800

    Add datastore

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -13,6 +13,8 @@
 // limitations under the License.
 
 package com.google.sps.servlets;
+
+import com.google.sps.data.Comment;
 import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +22,9 @@ import java.util.List;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,9 +35,19 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/comments")
 public final class DataServlet extends HttpServlet { 
-  private List<String> comments = new ArrayList<>();
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Comments").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    List<Comment> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      String comment = (String) entity.getProperty("Comment");
+      long timestamp = (long) entity.getProperty("Timestamp");
+
+      Comment input = new Comment(comment, timestamp);
+      comments.add(input);
+    }
     response.setContentType("application/json;");
     Gson gson = new Gson();
     String json = gson.toJson(comments);
@@ -41,14 +56,14 @@ public final class DataServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
       String comment = receiveComments(request, "text-input", "");
-      comments.add(comment);
-
-      Entity commentEntity = new Entity("Comments");
+      long timestamp = System.currentTimeMillis();
+      Entity commentEntity = new Entity("Comment");
       commentEntity.setProperty("Comment", comment);
+      commentEntity.setProperty("Timestamp", timestamp);
 
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       datastore.put(commentEntity);
-      
+
       response.sendRedirect("/index.html");
   }
   private String receiveComments(HttpServletRequest request, String name, String defaultValue) {
@@ -58,4 +73,4 @@ public final class DataServlet extends HttpServlet {
     }
     return value;
   }
-}
+}  
